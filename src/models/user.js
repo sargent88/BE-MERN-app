4753093230d1d8344c43f3ec293d6b5f3cca1e7e
Steen G Sargent <ssargent@nerdunited.com>
2025-03-17 19:10:32 -0600

    fix: add email validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
   places: [{ type: mongoose.Types.ObjectId, required: true, ref: "Place" }],
@@ -14,5 +15,7 @@ const userSchema = new Schema({
     get: (val) => val.toISOString().split("T")[0], // Format date as YYYY-MM-DD
   },
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
